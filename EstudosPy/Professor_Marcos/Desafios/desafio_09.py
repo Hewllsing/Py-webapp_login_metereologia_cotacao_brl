15db5289140741a873eff7714ec402cab71c2090
Formando <formando@iMac-de-Administrador-3.local>
2025-12-15 12:49:48 +0000

    update no desafio 9, aumentando o grau de dificuldade

--- EstudosPy/Professor_Marcos/Desafios/desafio_09.py
+++ EstudosPy/Professor_Marcos/Desafios/desafio_09.py
@@ -4,20 +4,22 @@ from openpyxl import Workbook, load_workbook
 # Carrega o arquivo Excel "produtos.xlsx"
 tabela = load_workbook("produtos.xlsx")
 
-# Seleciona a aba ativa da planilha excel
+# Seleciona a aba ativa da planilha Excel
 aba_ativa = tabela.active
 
 # Percorre todas as células da coluna A
 for celula in aba_ativa["A"]:
-    # Verifica se o valor da célula é "SPA"
+    # Verifica se o valor da célula da coluna A é "SPA"
     if celula.value == "SPA":
-        # Obtém o número da linha onde o valor foi encontrado
-        linha = celula.row
-        
-        # Atualiza o valor da coluna D na mesma "linha"
-        aba_ativa[f"D{linha}"] = 2.1
+        # Caso encontre "SPA", percorre todas as células da coluna C
+        for celula_c in aba_ativa["C"]:
+            # Verifica se o valor da célula da coluna C é "Serviço"
+            if celula_c.value == "Serviço":
+                # Obtém o número da linha onde "Serviço" foi encontrado
+                linha = celula_c.row
+                
+                # Atualiza o valor da coluna D na mesma linha
+                aba_ativa[f"D{linha}"] = 2.1
 
 # Salva as alterações em um novo arquivo Excel
 tabela.save("produto_openpyxl.xlsx")
-
-
