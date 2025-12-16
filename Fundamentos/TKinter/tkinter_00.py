81a4e186c95e4432dc2c07b87c8ecc5f1a3bef50
Formando <formando@iMac-de-Administrador-3.local>
2025-12-16 16:26:44 +0000

    tkinter lib starter

---
+++ Fundamentos/TKinter/tkinter_00.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+menu_inicial = tk.Tk() # Criar a janela
+
+menu_inicial.title("Primeira app") # Nome da janela
+
+menu_inicial.geometry("500x500") # Largura/Altura
+
+menu_inicial.mainloop() # Manter a janela aberta
+
+menu_inicial.iconbitmap("src/icon.ico")
